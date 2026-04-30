c0b0781279c4e17ddd1945c805fe43772b023595
Ravindranath <shortsvideosh05@gmail.com>
2026-04-30 14:38:09 -0500

    folder structure added

---
+++ template.sh
@@ -0,0 +1,9 @@
+mkdir backend
+
+touch backend/main.py
+touch backend/models.py
+touch backend/prompts.py
+touch backend/requirements.txt
+touch backend/.env.example
+
+echo "This file is completed"
